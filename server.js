ee9da965e003c1a7855b1a939f6df618ee038ad6
Saptarshi Nath <saptarshinath@MacBook-Pro-5.local>
2024-12-30 00:35:21 +0530

    Update vercel, server, and package configuration for deployment

--- server.js
+++ server.js
@@ -126,7 +126,5 @@ app.use("/cors-anywhere/*", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
-});
-
 
 export default app;
